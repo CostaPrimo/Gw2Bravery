9185c247dae6cad5f273a129c8266489f828d815
CostaPrimo <jeppeenevold@gmail.com>
2026-04-04 19:23:20 +0200

    added V2 to backend + begun gw2 Api testing

--- UltimateBravery/Backend.scala
+++ UltimateBravery/Backend.scala
@@ -1,23 +1,63 @@
 package UltimateBravery
 
+import UltimateBravery.src.main.Aggregates.BuildV2
 import UltimateBravery.src.main.Build
+import UltimateBravery.src.main.ClassSpecific.Classes
 import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
+import org.json.JSONArray
 
 import java.io.OutputStream
-import java.net.InetSocketAddress
+import java.net.{InetSocketAddress, URI, URLEncoder}
+import java.net.http.{HttpClient, HttpRequest, HttpResponse}
 
 object Backend {
 
-  def main(args: Array[String]): Unit = {
+  private val CLASSES = new Classes
 
+  def main(args: Array[String]): Unit = {
     val server: HttpServer = HttpServer.create(new InetSocketAddress(8080), 0)
     server.createContext("/ultimatebravery/simple", new SimpleHandler())
     server.createContext("/ultimatebravery/complex", new ComplexHandler())
+    server.createContext("/v2/ultimatebravery/complex", new ComplexHandlerV2())
     server.createContext("/ultimatebravery/pvp", new PvPHandler())
+    server.createContext("/apitest", new ApiHandler())
     server.setExecutor(null)
     server.start()
 
     println("Server Started")
+  }
+
+  class ApiHandler extends HttpHandler {
+    override def handle(exchange: HttpExchange): Unit = {
+      println("Received api request")
+      val api_key = ""
+      val charName = URLEncoder.encode("", "UTF-8")
+
+      val auth = String.format("Bearer %s", api_key)
+      val path = String.format("https://api.guildwars2.com/v2/characters?id=%s", charName)
+      // https://api.guildwars2.com/v2/characters/%s/skills
+      // https://api.guildwars2.com/v2/characters/%s/specializations
+      val client: HttpClient = HttpClient.newHttpClient()
+
+      try {
+        val request: HttpRequest = HttpRequest.newBuilder()
+          .GET()
+          .uri(URI.create(path))
+          .header("Authorization", auth)
+          .build()
+        println("Sending request")
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        val body: JSONArray = new JSONArray(response.body())
+      } catch {
+        case e: Exception => println(e)
+      }
+
+      exchange.getResponseHeaders.set("Access-Control-Allow-Origin", "*")
+      exchange.sendResponseHeaders(200, "OK".length)
+      val os: OutputStream = exchange.getResponseBody
+      os.write("OK".getBytes())
+      os.close()
+    }
   }
 
   class SimpleHandler extends HttpHandler {
@@ -55,6 +95,29 @@ object Backend {
       val chosenClass = extractClassFromQueryParams(query)
       val build = new Build(chosenClass)
       val response = build.iAmBrave()
+      exchange.getResponseHeaders.set("Access-Control-Allow-Origin", "*")
+      exchange.sendResponseHeaders(200, response.length)
+      val os: OutputStream = exchange.getResponseBody
+      os.write(response.getBytes)
+      os.close()
+    }
+  }
+
+  class ComplexHandlerV2 extends HttpHandler {
+    override def handle(exchange: HttpExchange): Unit = {
+      println("Received request")
+      val query = exchange.getRequestURI.getQuery
+      val chosenClass = extractClassFromQueryParams(query)
+      var classRoll = ""
+
+      if (CLASSES.getAllClasses.contains(chosenClass)) {
+        classRoll = chosenClass
+      } else {
+        classRoll = CLASSES.ultimateBravery
+      }
+
+      val build = new BuildV2(classRoll)
+      val response = build.getJsonObject.toString
       exchange.getResponseHeaders.set("Access-Control-Allow-Origin", "*")
       exchange.sendResponseHeaders(200, response.length)
       val os: OutputStream = exchange.getResponseBody
